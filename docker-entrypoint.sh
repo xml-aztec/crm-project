743c17b0deb9f6c4ee42f59ceb5e666a7bee5d64
xml-aztec <asankozhoevn@gmail.com>
2025-08-08 12:43:41 +0600

    fix: исправить путь к приложению в скрипте запуска для корректного запуска backend

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,8 @@
 . /venv/bin/activate
 
 # Запускаем backend в фоне
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+cd backend
+uvicorn app.main:app --host 0.0.0.0 --port 8000
 
 # Запускаем nginx в фореграунде
 nginx -g 'daemon off;'
