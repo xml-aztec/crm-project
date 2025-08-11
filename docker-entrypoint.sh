7ecdc53c8677763d10da345c463e4d9dff616067
xml-aztec <asankozhoevn@gmail.com>
2025-08-11 21:56:35 +0600

    fix: исправить запуск backend в фоновом режиме и обновить путь к статике в конфигурации Nginx

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,7 +5,7 @@
 
 # Запускаем backend в фоне
 cd backend
-uvicorn app.main:app --host 0.0.0.0 --port 8000
+uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
 # Запускаем nginx в фореграунде
 nginx -g 'daemon off;'
